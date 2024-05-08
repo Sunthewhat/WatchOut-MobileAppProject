2599905a9971b6ee27feb623daf2f0c51744b461
Chotiwet Wisitworanat <chotiwet.wisi@icloud.com>
2024-05-09 02:00:16 +0700

    change env to production

--- app/lib/constant/environment.dart
+++ app/lib/constant/environment.dart
@@ -1,5 +1,5 @@
 class EnvironmentConstant {
-  static String baseurl = "http://10.5.100.83:8000";
+  static String baseurl = "https://watchout.sunthewhat.com";
   static String isFirstTime = 'seen';
   static String userToken = 'user_token';
 }
