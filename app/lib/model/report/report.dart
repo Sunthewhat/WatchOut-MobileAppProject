a0d4e037e91ce20a029f27f0b566c5890cf09772
Chotiwet Wisitworanat <chotiwet.wisi@icloud.com>
2024-05-16 01:50:35 +0700

    change report Type

--- app/lib/model/report/report.dart
+++ app/lib/model/report/report.dart
@@ -1,37 +1,40 @@
 class ReportResponse {
   int id;
-  int imageId;
-  int userId;
   String title;
   String description;
+  String image;
   String type;
   DateTime time;
   double latitude;
   double longitude;
+  String user;
+  String userImage;
 
   ReportResponse({
     required this.id,
-    required this.imageId,
-    required this.userId,
     required this.title,
     required this.description,
     required this.type,
     required this.time,
     required this.latitude,
     required this.longitude,
+    required this.user,
+    required this.userImage,
+    required this.image,
   });
 
   factory ReportResponse.fromJson(Map<String, dynamic> json) {
     return ReportResponse(
       id: json['id'],
-      imageId: json['imageId'],
-      userId: json['userId'],
       title: json['title'],
       description: json['description'],
       type: json['type'],
       time: DateTime.parse(json['time']),
       latitude: double.parse(json['latitude']),
       longitude: double.parse(json['longitude']),
+      user: json['user'],
+      userImage: json['user_image'],
+      image: json['image'],
     );
   }
 }
